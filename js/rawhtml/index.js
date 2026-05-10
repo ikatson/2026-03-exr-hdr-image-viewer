94b9606e0a1a9e92ed2985f6ae4ba7ad56557c4b
Igor Katson <igor.katson@gmail.com>
2026-05-10 11:20:26 +0100

    Saving JS

---
+++ js/rawhtml/index.js
@@ -0,0 +1,82 @@
+// @ts-check
+
+function hslGradient() {
+  /** @type {HTMLCanvasElement} */
+  // @ts-ignore
+  let canvas = document.getElementById("hsl-gradient");
+  const W = 1024;
+  const H = 100;
+  canvas.width = W;
+  canvas.height = H;
+  /** @type {CanvasRenderingContext2D} */
+  // @ts-ignore
+  let c = canvas.getContext("2d");
+  for (let i = 0; i < 256; i++) {
+    let fcolor = i / 255;
+    let brightness = `${fcolor * 100}%`;
+    console.log(brightness);
+    c.fillStyle = `hsl(0 0 ${brightness})`;
+    c.fillRect((i / 256) * W, 0, ((i + 1) / 256) * W, H);
+  }
+}
+
+function rgbFullGradient() {
+  /** @type {HTMLCanvasElement} */
+  // @ts-ignore
+  let canvas = document.getElementById("rgb-0-255-gradient");
+  const W = 1024;
+  const H = 100;
+  canvas.width = W;
+  canvas.height = H;
+  /** @type {CanvasRenderingContext2D} */
+  // @ts-ignore
+  let c = canvas.getContext("2d");
+  for (let i = 0; i < 256; i++) {
+    let fcolor = i / 255;
+    let color = Math.floor(fcolor * 255);
+    c.fillStyle = `rgb(${color}, ${color}, ${color})`;
+    c.fillRect((i / 256) * W, 0, ((i + 1) / 256) * W, H);
+  }
+}
+
+function simulatedGradient() {
+  /** @type {HTMLCanvasElement} */
+  // @ts-ignore
+  let canvas = document.getElementById("simulated-gradient");
+  const W = 1024;
+  const H = 100;
+  canvas.width = W;
+  canvas.height = H;
+  /** @type {CanvasRenderingContext2D} */
+  // @ts-ignore
+  let c = canvas.getContext("2d");
+  for (let i = 0; i < 256; i++) {
+    let fcolor = i / 255;
+    // srgb color would be this
+    let color = Math.floor(fcolor * 255);
+    // linear brightness
+    let linearBrightness = Math.pow(color / 255, 2.2);
+    // truncate linear brightness
+    color = Math.floor(linearBrightness * 255) / 255;
+    // convert back to srgb
+    color = Math.pow(color, 1 / 2.2);
+    // truncate again to u8
+    color = Math.floor(color * 255);
+
+    c.fillStyle = `rgb(${color}, ${color}, ${color})`;
+    c.fillRect((i / 256) * W, 0, ((i + 1) / 256) * W, H);
+  }
+}
+
+function main() {
+  hslGradient();
+  rgbFullGradient();
+  simulatedGradient();
+}
+
+try {
+  main();
+} catch (e) {
+  document.getElementById("error").textContent = `error: ${e}`;
+  throw e;
+}
